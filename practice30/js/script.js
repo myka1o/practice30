86e6033a86d3de6af7b31a855342eccd5f123abb
Ivan_k1va <dubrovin.i.a@mail.ru>
2020-09-14 23:53:03 +0300

    Events 5:5

--- practice30/js/script.js
+++ practice30/js/script.js
@@ -22,52 +22,104 @@ const movieDB = {
         "Одержимость",
         "Скотт Пилигрим против..."
     ],
-    UpdMovList: function() {
-        let movieList = document.querySelectorAll('.promo__interactive-list');
-        movieList[0].innerHTML = '';
+    UpdMovList: function (movies, parent) {
+        parent.innerHTML = '';
 
-        movieDB.movies.sort();
-        
-        movieDB.movies.forEach((film, i) => {
-            movieList[0].innerHTML += `
+        sortArr(movies);
+
+        movies.forEach((film, i) => {
+            parent.innerHTML += `
             <li class="promo__interactive-item">${i + 1}: ${film}
                 <div class="delete"></div>
             </li>
             `;
         });
 
+        document.querySelectorAll('.delete').forEach((btn, i) => {
+            btn.addEventListener('click', () => {
+                btn.parentElement.remove();
+                movieDB.movies.splice(i, 1);
+
+                movieDB.UpdMovList(movies, parent);
+            })
+        });
     }
 
 };
 
-let div_promo__adv_s = document.getElementsByClassName('promo__adv');
-div_promo__adv_s[0].remove();
+const adv = document.querySelectorAll('.promo__adv img');
+const advText = document.querySelectorAll('.promo__adv-title');
+const deleteAdv = (arr) => {
+    arr.forEach(item => {
+        item.remove();
+    });
+};
 
+deleteAdv(adv);
+deleteAdv(advText);
 
 [].forEach.call(document.getElementsByClassName('promo__genre'), function (div_promo__genre) {
     div_promo__genre.innerText = "драма";
 });
 
-let class_promo__content_s = document.querySelectorAll('.promo__content');
-let class_promo__content_promo__bg_s = class_promo__content_s[0].querySelector('.promo__bg');
-class_promo__content_promo__bg_s.style.backgroundImage = "url('/img/bg.jpg')";
-
-movieDB.UpdMovList();
-//События
-
-const btn = document.querySelector('button');
-const ClickAddMovie = (event) => {
-    event.preventDefault();
-
-    const sslen = 21;
-    const input_adding__input = document.querySelector('.adding__input');
-    const movieName = input_adding__input.value.lenght <= sslen ? input_adding__input.value : input_adding__input.value.substr(0, sslen) + "...";
-
-    movieDB.movies.push(movieName);
-    movieDB.UpdMovList();
+const changBG = () => {
+    let class_promo__content_s = document.querySelectorAll('.promo__content');
+    let class_promo__content_promo__bg_s = class_promo__content_s[0].querySelector('.promo__bg');
+    class_promo__content_promo__bg_s.style.backgroundImage = "url('/img/bg.jpg')";
 };
 
-btn.addEventListener('click', ClickAddMovie);
+changBG();
+
+const sortArr = (arr) => {
+    arr.sort();
+};
+
+sortArr(movieDB.movies);
+
+let movieList = document.querySelectorAll('.promo__interactive-list');
+movieDB.UpdMovList(movieDB.movies, movieList[0]);
+
+//События
+const addForm = document.querySelector('form.add'),
+    addInput = addForm.querySelector('.adding__input'),
+    checkbox = addForm.querySelector('[type="checkbox"]');
+
+addForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    const newMovie = addInput.value;
+    const favorite = checkbox.checked;
+
+    if (newMovie) {
+
+        const sslen = 21;
+        const input_adding__input = document.querySelector('.adding__input');
+        const movieName = input_adding__input.value.lenght <= sslen ? input_adding__input.value : input_adding__input.value.substr(0, sslen) + "...";
+
+        movieDB.movies.push(newMovie);
+        movieDB.UpdMovList(movieDB.movies, movieList[0]);
+
+        if(favorite){
+            console.log('New favorite film added!');
+        }
+    }
+
+    event.target.reset();
+});
+
+// const btn = document.querySelector('button');
+// const ClickAddMovie = (event) => {
+//     event.preventDefault();
+
+//     const sslen = 21;
+//     const input_adding__input = document.querySelector('.adding__input');
+//     const movieName = input_adding__input.value.lenght <= sslen ? input_adding__input.value : input_adding__input.value.substr(0, sslen) + "...";
+
+//     movieDB.movies.push(movieName);
+//     movieDB.UpdMovList();
+// };
+
+// btn.addEventListener('click', ClickAddMovie);
 
 //Удаление из списка фильмов
 const promoInter_item = document.querySelector('.delete'); //'.promo__interactive-item');
@@ -75,4 +127,3 @@ const ClickPromoInter_item = (event) => {
     console.log(event);
 };
 promoInter_item.addEventListener('click', ClickPromoInter_item);
-
